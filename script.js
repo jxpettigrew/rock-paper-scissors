197767b9c8510fa405746cb842681674187ea70b
Jordan Pettigrew <jxpettigrew@gmail.com>
2023-01-29 22:43:13 -0600

    refactor game logic to work with UI

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
 function getComputerChoice() {
     randomInt = Math.floor(Math.random() * 3);
+    const computerText = document.querySelector('.computer-space .choice');
     switch (randomInt) {
         case 0:
-            return 'rock'
+            computerText.innerText = 'rock';
+            break;
         case 1:
-            return 'paper'
+            computerText.innerText = 'paper';
+            break;
         case 2:
-            return 'scissors'
+            computerText.innerText = 'scissors';
+            break;
     }
-    /*if (randomInt === 0) {
-        return 'rock';
-    } else if (randomInt === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }*/
 }
 
-function getPlayerChoice() {
-    //playerInput = prompt('Rock, paper, or scissors? Type your selection below:');
-    return playerInput.trim().toLowerCase();
+const playerChoice = function () {
+    const choice = this.innerHTML.toLowerCase();
+    const playerText = document.querySelector('.player-space .choice');
+    playerText.innerText = choice;
 }
+
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`You played ${playerSelection}. Computer played ${computerSelection}.`);
+    let gameUpdate = document.querySelector('.game-text');
 
     if (playerSelection === computerSelection) {
-        return 'Its a tie!';
+        gameUpdate.innerText = 'Its a tie!';
+        return;
     }
 
-    if (playerSelection == 'rock') {
-        if (computerSelection == 'paper') {
-            return 'You lose!';
-        } else {
-            return 'You win!';
-        }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'scissors') {
-            return 'You lose!';
-        } else {
-            return 'You win!';
-        }
+    if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        gameUpdate.innerText = 'Computer wins!';
     } else {
-        if (computerSelection == 'rock') {
-            return 'You lose!';
-        } else {
-            return 'You win!';
-        }
+        gameUpdate.innerText = 'You win!';
     }
 }
 
-function playGame(roundNumber) {
+
+function playGame() {
     let playerScore = 0;
     let computerScore = 0;
     let tieNumber = 0;
-    for (let i = 0; i < roundNumber; i++) {
+    let gameOver = false;
+    let rounds = 1
+
+    const buttonList = document.querySelectorAll('button');
+    for (let btn of buttonList) {
+        btn.addEventListener('click', playerChoice);
+        btn.addEventListener('click', getComputerChoice);
+        btn.addEventListener('click', function () {
+            playRound(document.querySelector('.player-space .choice').innerText,
+                document.querySelector('.computer-space .choice').innerText);
+        });
+        btn.addEventListener('click', function () { handleGameLogic() });
+        btn.addEventListener('click', () => rounds++);
+    }
+
+    /*for (let i = 0; i < roundNumber; i++) {
         let playerChoice = getPlayerChoice();
         let computerChoice = getComputerChoice();
         let result = playRound(playerChoice, computerChoice)
-
+        
         console.log(result);
         if (result == 'You win!') {
             playerScore++;
@@ -69,9 +73,31 @@ function playGame(roundNumber) {
         }
         console.log(`Player: ${playerScore} \n Computer: ${computerScore} \n Ties: ${tieNumber}`);
     }
-
-    console.log(`Final score:\n Player: ${playerScore} \n Computer: ${computerScore} \n Ties: ${tieNumber}`);
+    
+    console.log(`Final score:\n Player: ${playerScore} \n Computer: ${computerScore} \n Ties: ${tieNumber}`);*/
 }
 
-//let rounds = prompt('How many rounds do you want to play?');
-playGame(rounds);
+const handleGameLogic = function () {
+    let gameUpdate = document.querySelector('.game-text');
+    let playerScore = document.querySelector('.player-space span');
+    let computerScore = document.querySelector('.computer-space span');
+    console.log(playerScore, computerScore);
+    console.log(gameUpdate.innerText);
+    if (gameUpdate.innerText === 'You win!') {
+        let newScore = parseInt(playerScore.innerText) + 1;
+        playerScore.innerText = newScore.toString();
+    } else if (gameUpdate.innerText === 'Computer wins!') {
+        let newScore = parseInt(computerScore.innerText) + 1;
+        computerScore.innerText = newScore.toString();
+    }
+}
+
+
+
+const button = document.querySelector('.start-button');
+const controls = document.querySelector('.controls');
+button.addEventListener('click', function () {
+    button.style.display = "none";
+    controls.style.display = "inline";
+    playGame();
+});
