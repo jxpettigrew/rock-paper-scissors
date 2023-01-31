1f01c456ce000a6906b6232d415ce067fad7d00f
Jordan Pettigrew <jxpettigrew@gmail.com>
2023-01-31 00:50:38 -0600

    refactor game logic

--- script.js
+++ script.js
@@ -20,33 +20,54 @@ const playerChoice = function () {
     playerText.innerText = choice;
 }
 
+const checkScore = function () {
+    let gameUpdate = document.querySelector('.game-text');
+    let playerScore = Number(document.querySelector('.player-space span').innerText);
+    let computerScore = Number(document.querySelector('.computer-space span').innerText);
+
+    if (playerScore === 5 || computerScore === 5) {
+        gameUpdate.innerText = `Game over! ${playerScore > computerScore ? 'Player' : 'Computer'} wins!`;
+        document.querySelector('.new-game-button').style.display = 'inline';
+        document.querySelector('.controls').style.display = 'none';
+        document.querySelector('.new-game-button').addEventListener('click', function () { document.location.reload(true) });
+    }
+}
+
 
 function playRound(playerSelection, computerSelection) {
     let gameUpdate = document.querySelector('.game-text');
+    let playerScore = document.querySelector('.player-space span');
+    let computerScore = document.querySelector('.computer-space span');
 
     if (playerSelection === computerSelection) {
         gameUpdate.innerText = 'Its a tie!';
         return;
     }
-
     if ((playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'paper' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'rock')) {
         gameUpdate.innerText = 'Computer wins!';
+        let newScore = parseInt(computerScore.innerText) + 1;
+        computerScore.innerText = newScore.toString();
     } else {
         gameUpdate.innerText = 'You win!';
+        let newScore = parseInt(playerScore.innerText) + 1;
+        playerScore.innerText = newScore.toString();
     }
+    checkScore();
+
 }
 
+const resetGame = function () {
+    document.querySelector('.game-text').innerText = '';
+    document.querySelector('.player-space span').innerText = '0';
+    document.querySelector('.computer-space span').innerText = '0';
+    document.querySelector('.player-space .choice').innerText = '';
+    document.querySelector('.computer-space .choice').innerText = '';
+}
 
 function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieNumber = 0;
-    let gameOver = false;
-    let rounds = 1
-
-    const buttonList = document.querySelectorAll('button');
+    const buttonList = document.querySelectorAll('.controls button');
     for (let btn of buttonList) {
         btn.addEventListener('click', playerChoice);
         btn.addEventListener('click', getComputerChoice);
@@ -54,50 +75,14 @@ function playGame() {
             playRound(document.querySelector('.player-space .choice').innerText,
                 document.querySelector('.computer-space .choice').innerText);
         });
-        btn.addEventListener('click', function () { handleGameLogic() });
-        btn.addEventListener('click', () => rounds++);
-    }
-
-    /*for (let i = 0; i < roundNumber; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice)
-        
-        console.log(result);
-        if (result == 'You win!') {
-            playerScore++;
-        } else if (result == "You lose!") {
-            computerScore++;
-        } else {
-            tieNumber++;
-        }
-        console.log(`Player: ${playerScore} \n Computer: ${computerScore} \n Ties: ${tieNumber}`);
-    }
-    
-    console.log(`Final score:\n Player: ${playerScore} \n Computer: ${computerScore} \n Ties: ${tieNumber}`);*/
-}
-
-const handleGameLogic = function () {
-    let gameUpdate = document.querySelector('.game-text');
-    let playerScore = document.querySelector('.player-space span');
-    let computerScore = document.querySelector('.computer-space span');
-    console.log(playerScore, computerScore);
-    console.log(gameUpdate.innerText);
-    if (gameUpdate.innerText === 'You win!') {
-        let newScore = parseInt(playerScore.innerText) + 1;
-        playerScore.innerText = newScore.toString();
-    } else if (gameUpdate.innerText === 'Computer wins!') {
-        let newScore = parseInt(computerScore.innerText) + 1;
-        computerScore.innerText = newScore.toString();
     }
 }
-
-
 
 const button = document.querySelector('.start-button');
 const controls = document.querySelector('.controls');
 button.addEventListener('click', function () {
     button.style.display = "none";
     controls.style.display = "inline";
+    resetGame();
     playGame();
 });
